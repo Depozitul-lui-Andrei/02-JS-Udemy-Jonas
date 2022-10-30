509b01e6bf67c036665dea0f7b6b26d1e2db2327
Andrei <andreisolcan@protonmail.com>
2022-10-30 18:35:34 +0200

    1

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,11 @@
+//Values and Variables
+
+let country = "Romania";
+let continent = "Europe";
+let population = 15;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//
