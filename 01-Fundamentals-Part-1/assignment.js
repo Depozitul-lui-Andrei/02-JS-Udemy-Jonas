3ebee20435f420ee56705bbc18fb0875420e7a86
Andrei <andreisolcan@protonmail.com>
2022-10-31 21:06:47 +0200

    1

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,14 +1,13 @@
-/*
 //Values and Variables
 console.log("###$$$ASSIGNMENTS FILE");
-let country = "Romania";
-let continent = "Europe";
+const country = "Romania";
+const continent = "Europe";
 let population = 15;
 
 console.log(country);
 console.log(continent);
 console.log(population);
-
+/*
 //random attempts
 console.log("TOYING WITH JS");
 class Shape {
@@ -22,9 +21,25 @@ const rect = new Rectangle(3, 4);
 console.log(rect);
 */
 
-// let isIsland = false;
-// let language;
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+language = "romanian";
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description1 =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description1);
