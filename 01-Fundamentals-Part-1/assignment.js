859fc3dbda2484de5235b2bfc296f471dd93f6e9
Andrei <andreisolcan@protonmail.com>
2022-10-31 20:21:21 +0200

    1

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,5 +1,6 @@
+/*
 //Values and Variables
-
+console.log("###$$$ASSIGNMENTS FILE");
 let country = "Romania";
 let continent = "Europe";
 let population = 15;
@@ -8,4 +9,22 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-//
+//random attempts
+console.log("TOYING WITH JS");
+class Shape {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+class Rectangle extends Shape {}
+const rect = new Rectangle(3, 4);
+console.log(rect);
+*/
+
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
