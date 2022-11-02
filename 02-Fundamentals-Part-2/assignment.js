c0dbb20041cac78d8bb7d7070b6a0919ab1aa0b7
Andrei <andreisolcan@protonmail.com>
2022-11-02 21:15:58 +0200

    1

--- 02-Fundamentals-Part-2/assignment.js
+++ 02-Fundamentals-Part-2/assignment.js
@@ -12,7 +12,7 @@ function describeCountry(country, population, capitalCity) {
   return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
 }
 
-const country1 = describeCountry(`Germany`, 35, `Berlin`);
+const country1 = describeCountry(`Germany`, 83, `Berlin`);
 const country2 = describeCountry(`France`, 43, `Paris`);
 const country3 = describeCountry(`UK`, 23, `London`);
 console.log(country1);
@@ -63,3 +63,26 @@ const romania3 = percentageOfWorld3(16);
 const usa3 = percentageOfWorld3(355);
 console.log(china3, romania3, usa3);
 */
+
+`LECTURE: Functions Calling Other Functions
+1. Create a function called 'describePopulation'. Use the function type you 
+like the most. This function takes in two arguments: 'country' and 
+'population', and returns a string like this: 'China has 1441 million people, 
+which is about 18.2% of the world.'
+2. To calculate the percentage, 'describePopulation' call the 
+'percentageOfWorld1' you created earlier
+3. Call 'describePopulation' with data for 3 countries of your choice`;
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+function describePopulation(country, population) {
+  const percentageCountry = percentageOfWorld1(population);
+  const description = `${country} has ${population} million people, wich is about ${percentageCountry}% of the world.`;
+  return description;
+}
+
+console.log(describePopulation(`Romania`, 16));
+console.log(describePopulation(`Germania`, 83));
+console.log(describePopulation(`USA`, 350));
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -64,4 +64,19 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 };
 console.log(yearsUntilRetirement(1991, `Jonas`));
 console.log(yearsUntilRetirement(1988, `Bob`));
+
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
 */
