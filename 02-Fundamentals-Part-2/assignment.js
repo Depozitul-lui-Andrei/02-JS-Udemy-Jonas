7274aa2b86617eb3240165ec14bd41187f74a0de
Andrei <andreisolcan@protonmail.com>
2022-11-02 19:35:36 +0200

    1

---
+++ 02-Fundamentals-Part-2/assignment.js
@@ -0,0 +1,165 @@
+/*
+//Values and Variables
+console.log("###$$$ASSIGNMENTS FILE");
+const country = "Romania";
+const continent = "Europe";
+let population = 15;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+language = "romanian";
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description1 =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description1);
+
+const description2 = `${country} is in ${continent}, and it's ${population} million people speak ${language}`;
+console.log(description2);
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+}
+
+//1. Predict the result of these 5 operations without executing them:
+"9" - "5"; // 4
+"19" - "13" + "17"; // 617
+"19" - "13" + 17; // 23
+"123" < 57; // false
+5 + 6 + "4" + 9 - 4 - 2; // 117 (correct answer is 1143 because 5 + 6 + '4' = '114' and then 9 - 4 - 2 = 3; numbers before string '4' become strings, then the subtractions happen, then string + number)
+//2. Execute the operations to check if you were right
+
+
+// 1. Declare a variable 'numNeighbours' based on a prompt input like this:
+// prompt('How many neighbour countries does your country
+// have?');
+// 2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+// == for now)
+// 3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+// is greater than 1
+// 4. Use an else block to log 'No borders' (this block will be executed when
+// 'numNeighbours' is 0 or any other value)
+// 5. Test the code with different values of 'numNeighbours', including 1 and 0.
+// 6. Change == to ===, and test the code again, with the same values of
+// 'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+// is this happening?
+// 7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+// when you input 1
+// 8. Reflect on why we should use the === operator and type conversion in this
+// situation
+
+const numNeighbours = Number(
+  prompt(
+    `How many neighbour countries does your country 
+  have?`
+  )
+);
+
+if (numNeighbours === 1) {
+  console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+  console.log(`More than 1 border`);
+} else {
+  console.log(`No borders`);
+}
+
+
+`1. Comment out the previous code so the prompt doesn't get in the way
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a 
+country that speaks english, has less than 50 million people and is not an 
+island.
+3. Write an if statement to help Sarah figure out if your country is right for her. 
+You will need to write a condition that accounts for all of Sarah's criteria. Take 
+your time with this, and check part of the solution if necessary.
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If 
+not, log 'Portugal does not meet your criteria :('
+5. Probably your country does not meet all the criteria. So go back and temporarily 
+change some variables in order to make the condition true (unless you live in 
+Canada :D`;
+
+const language = "romanian";
+let population = 15;
+const isIsland = false;
+const country = "Romania";
+
+if (language === "english" && population < 50 && isIsland === false) {
+  console.log(`You should live in ${country} :)`);
+} else {
+  console.log(`${country} does not meet your criteria :(`);
+}
+
+
+`1. Use a switch statement to log the following string for the given 'language':
+chinese or mandarin: 'MOST number of native speakers!'
+spanish: '2nd place in number of native speakers'
+english: '3rd place'
+hindi: 'Number 4'
+arabic: '5th most spoken language'
+for all other simply log 'Great language too :D'`;
+
+const language = `romanian`;
+
+switch (language) {
+  case `chinese`:
+  case `mandarin`:
+    console.log(`MOST number of native speakers!`);
+    break;
+  case `spanish`:
+    console.log(`2nd place in number of native speakers`);
+    break;
+  case `english`:
+    console.log(`3rd place`);
+    break;
+  case `hindi`:
+    console.log(`Number 4`);
+    break;
+  case `arabic`:
+    console.log(`5th most spoken language`);
+    break;
+  default:
+    console.log(`Great language too :D`);
+}
+
+`1. If your country's population is greater than 33 million, use the ternary operator 
+to log a string like this to the console: 'Portugal's population is above average'. 
+Otherwise, simply log 'Portugal's population is below average'. Notice how only
+one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to 
+130. See the different results, and set the population back to original`;
+
+let population = 15;
+const country = "Romania";
+console.log(
+  `${
+    population > 33
+      ? `Romania's population is above average`
+      : `Romania's population is below average`
+  }`
+);
+
+console.log(
+  `${country}'s population is ${population > 33 ? `above` : `below`} average `
+);
+*/
---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive :D`);
+
+// const interface = `Audio`;
+// const private = 43;
+// const if = 23;
+*/
+
+function logger() {
+  console.log(`My name is Jonas`);
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
