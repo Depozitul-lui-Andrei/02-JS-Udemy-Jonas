4ecadb430c81f7bf30bc6cbbbe8249aabce26345
Andrei <andreisolcan@protonmail.com>
2022-11-03 22:13:47 +0200

    1

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -100,7 +100,7 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, `Jonas`));
 console.log(yearsUntilRetirement(1950, `Mike`));
-*/
+
 
 const friend1 = `Michael`;
 const friend2 = `Steven`;
@@ -146,3 +146,35 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
+*/
+
+const friends = [`Michael`, `Steven`, `Peter`];
+
+// Add elements
+const newLength = friends.push(`Jay`); // add to end
+console.log(friends);
+console.log(newLength);
+
+friends.unshift(`John`); // add to start
+console.log(friends);
+
+// Remove elements
+friends.pop(); // last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // first
+console.log(friends);
+
+console.log(friends.indexOf(`Steven`));
+console.log(friends.indexOf(`Bob`));
+
+friends.push(23);
+console.log(friends.includes(`Steven`));
+console.log(friends.includes(`Bob`));
+console.log(friends.includes(`23`));
+
+if (friends.includes(`Peter`)) {
+  console.log(`You have a friend called Peter`);
+}
