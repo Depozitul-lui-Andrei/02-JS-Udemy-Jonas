eeeef333e2bbad53f6faf3a9480f39a1b99ecfbc
Andrei <andreisolcan@protonmail.com>
2022-11-03 20:36:45 +0200

    1

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -109,7 +109,40 @@ const friend3 = `Peter`;
 const friends = [`Michael`, `Steven`, `Peter`];
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
-console.log(years);
+const y = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
 
 console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+console.log(friends.length); // 3
+
+console.log(friends[friends.length - 1]); //automatic retrieval of las array element
+
+friends[2] = `Jay`;
+console.log(friends);
+// friends = [`Bob`, `Alice`]; // wrong - redeclaring const
+
+const firstName = `Jonas`;
+const jonas = [firstName, `Schmedtmann`, 2037 - 1991, `teacher`, friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
