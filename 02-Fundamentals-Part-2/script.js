05ffa5cf8fff1bba2e826737dd06a989a35b5566
Andrei Solcan <andreisolcan@protonmail.com>
2022-11-05 19:04:24 +0200

    2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -224,7 +224,7 @@ console.log(jonas);
 console.log(
   `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}`
 );
-*/
+
 
 const jonas = {
   firstName: `Jonas`,
@@ -234,7 +234,33 @@ const jonas = {
   friends: [`Michael`, `Steven`, `Peter`],
   hasDriverLicense: true,
 
-  calcAge: function (birthYear) {
-    return 2037 - birthYear;
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
   },
 };
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// Challenge
+// `Jonas is a 46-year old teacher and he has a drivers license`
+jonas.getSummary = function () {
+  return `${this.firstName} is a ${this.calcAge()}-year old ${
+    this.job
+  } and he has ${this.hasDriverLicense ? `a` : `no`} driver's license`;
+};
+console.log(jonas.getSummary());
+*/
