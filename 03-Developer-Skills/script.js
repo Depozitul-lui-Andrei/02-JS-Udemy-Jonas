5a5af56a0f7325828066d87eaba3e79242ecd772
Andrei <andreisolcan@protonmail.com>
2022-11-06 21:29:43 +0200

    3

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -2,7 +2,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 
 //TODO HIGHLIGHTS MUST BE CONFIGURED
-
+/*
 const temperatures = [3, -2, -6, -1, `error`, 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem
@@ -57,3 +57,47 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+
+const measureKelvin = function () {
+  const measurement = {
+    type: `temp`,
+    unit: `celsius`,
+    // C) FIX
+    // value: Number(prompt(`Degrees celsius:`)),
+    value: 10,
+  };
+  // B) find the bug
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) identify the bug
+console.log(measureKelvin());
+
+// using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== `number`) continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) identify
+console.log(amplitudeBug);
+*/
