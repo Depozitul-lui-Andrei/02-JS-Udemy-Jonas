69609339fa47e02bfc9ced61540064a29027d2c8
Andrei Solcan <andreisolcan@protonmail.com>
2022-11-12 20:24:44 +0200

    7

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -20,10 +20,18 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
 
+  // when there is no input
   if (!guess) {
     document.querySelector(`.message`).textContent = `⛔ No number!`;
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+
+    document.querySelector(`.number`).style.width = `30rem`;
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `📈 Too High!`;
@@ -32,6 +40,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     } else {
       document.querySelector(`.message`).textContent = `💥 You lost the game!`;
     }
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `📉 Too Low!`;
