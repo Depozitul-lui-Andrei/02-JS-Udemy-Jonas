2ac870f700dc6fe5bdc1b523f2b52a5f6e21be52
Andrei <andreisolcan@protonmail.com>
2022-11-14 23:29:41 +0200

    6

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,4 +1,5 @@
 'use strict';
+// working with classes lesson and how to write DRY code
 
 const modal = document.querySelector(`.modal`);
 const overlay = document.querySelector(`.overlay`);
@@ -6,5 +7,19 @@ const btnCloseModal = document.querySelector(`.close-modal`);
 const btnsOpenModal = document.querySelectorAll(`.show-modal`);
 console.log(btnsOpenModal);
 
+const openModal = function () {
+  console.log(`Button clicked`);
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener(`click`, openModal);
+
+btnCloseModal.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`, closeModal);
