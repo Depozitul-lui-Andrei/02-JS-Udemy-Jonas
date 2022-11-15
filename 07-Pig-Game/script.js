dc7c2476a1403023157d4c70003eae598f60b278
Andrei <andreisolcan@protonmail.com>
2022-11-15 21:46:56 +0200

    7

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -13,36 +13,102 @@ const btnNew = document.querySelector(`.btn--new`);
 const btnRoll = document.querySelector(`.btn--roll`);
 const btnHold = document.querySelector(`.btn--hold`);
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEL.classList.add(`hidden`);
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEL.classList.add(`hidden`);
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player0El.classList.add(`player--active`);
+  player1El.classList.remove(`player--active`);
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle(`player--active`);
+  player1El.classList.toggle(`player--active`);
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener(`click`, function () {
-  // 1.Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // 1.Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // 2. Display the dice
-  diceEL.classList.remove(`hidden`);
-  diceEL.src = `dice-${dice}.png`;
+    // 2. Display the dice
+    diceEL.classList.remove(`hidden`);
+    diceEL.src = `dice-${dice}.png`;
 
-  // 3. Check for a rolled 1: if true
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle(`player--active`);
-    player1El.classList.toggle(`player--active`);
+    // 3. Check for a rolled 1: if true
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
+
+btnHold.addEventListener(`click`, function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >=100
+    if (scores[activePlayer] >= 100) {
+      // Finish game
+      playing = false;
+      diceEL.classList.add(`hidden`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add(`player--winner`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+// New game button
+btnNew.addEventListener(`click`, init);
+
+/*
+// My way of doing it (challenge)
+btnNew.addEventListener(`click`, function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEL.classList.add(`hidden`);
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove(`player--winner`);
+  document.querySelector(`.player--0`).classList.add(`player--active`);
+  activePlayer = 0;
+  playing = true;
+});
+*/
