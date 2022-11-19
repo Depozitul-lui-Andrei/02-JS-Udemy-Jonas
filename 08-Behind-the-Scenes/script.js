cdc8fdc9c7daca42b418ac419efde1a7af1ed44d
Andrei <andreisolcan@protonmail.com>
2022-11-19 21:42:06 +0200

    8

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,2 +1,20 @@
 'use strict';
-// video - scope and the scope chain
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = `Jonas`;
+calcAge(1991);
+
+// console.log(age);
+// printAge();
