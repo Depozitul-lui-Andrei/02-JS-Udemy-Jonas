0f0033b7920ed4570efc375fc72c0859074edda7
Andrei <andreisolcan@protonmail.com>
2022-11-22 22:46:48 +0200

    8

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -178,6 +178,7 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8);
 */
 
+/*
 let age = 30;
 let oldAge = age;
 age = 31;
@@ -192,3 +193,39 @@ const friend = me;
 friend.age = 27;
 console.log(`Friend: `, friend); // name: 'Jonas', age: 27
 console.log(`Me: `, me); // name: 'Jonas', age: 27
+*/
+
+let lastName = `Williams`;
+let oldLastName = lastName;
+lastName = `Davis`;
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: `Jessica`,
+  lastName: `Williams`,
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = `Davis`;
+console.log(`Before marriage: `, jessica);
+console.log(`After marriage: `, marriedJessica);
+
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: `Jessica`,
+  lastName: `Williams`,
+  age: 27,
+  family: [`Alice`, `Bob`],
+};
+
+// Object.assign - shallow copy, not a deep copy (objects inside object)
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = `Davis`;
+
+jessicaCopy.family.push(`Mary`);
+jessicaCopy.family.push(`John`);
+
+console.log(`Before marriage: `, jessica2); // both have array with 4 values
+console.log(`After marriage: `, jessicaCopy);
