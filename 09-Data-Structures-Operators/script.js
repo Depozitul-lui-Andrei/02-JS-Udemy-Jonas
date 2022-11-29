189184070ac7ca5cefa48e37b3cec7b0caf67cc7
Andrei <andreisolcan@protonmail.com>
2022-11-29 23:49:45 +0200

    9

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -58,6 +58,30 @@ const restaurant = {
 /////////////////////////////////////////////////
 // Strings part 1
 
+const airline = `TAP Air Portugal`;
+const plane = `A320`;
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(`B737`[0]);
+
+console.log(airline.length);
+console.log(`B737`.length);
+
+console.log(airline.indexOf(`r`));
+console.log(airline.lastIndexOf(`r`));
+console.log(airline.indexOf(`Portugal`));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(` `)));
+console.log(airline.slice(airline.lastIndexOf(` `) + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
 /////////////////////////////////////////////////
 // Summary: which data structure to use?
 
