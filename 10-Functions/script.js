001d838f20a3aab7331a3c1590ac5c0d0ce0dfad
Andrei Solcan <andreisolcan@protonmail.com>
2022-12-03 00:22:47 +0200

    10

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -62,3 +62,37 @@ newPassport(jonas);
 checkIn(flight, jonas);
 // passing by value vs passing by refference -> js has ONLY passing by value
 */
+
+// First-class and higher-order functions
+//
+
+/*
+// Functions accepting callback functions
+const oneWord = function (str) {
+  return str.replaceAll(` `, ``).toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(` `);
+  return [first.toUpperCase(), ...others].join(` `);
+};
+
+// Higher-order function with callback function inside
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer(`JavaScript is the best!`, upperFirstWord);
+transformer(`JavaScript is the best!`, oneWord);
+
+// JS callbacks all the time
+const high5 = function () {
+  console.log(`👋`);
+};
+document.body.addEventListener(`click`, high5);
+[`Jonas`, `Martha`, `Adam`].forEach(high5);
+
+// Abstraction = a way to reduce complexity and allow efficient design and implementation in complex software systems
+*/
