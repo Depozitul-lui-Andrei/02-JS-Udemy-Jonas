8216ec6f8f00c1d756e71bd7b388c61ab1d3a17f
Andrei <andreisolcan@protonmail.com>
2022-12-04 23:23:27 +0200

    10

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -240,4 +240,65 @@ runOnce();
 // console.log(isPrivate); // not working
 */
 
+/*
 // Closures
+const secureBooking = function () {
+  let passengerCoung = 0;
+
+  return function () {
+    passengerCoung++;
+    console.log(`${passengerCoung} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+// More closure examples
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 899;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+// setTimeout(function () {
+//   console.log(`Timer`);
+// }, 1000); // 1 sec delay
+*/
