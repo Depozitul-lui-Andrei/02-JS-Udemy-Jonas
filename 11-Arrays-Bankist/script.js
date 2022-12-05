dd97b0a7d52d2df5210a40c52cca6cecbed7d514
Andrei <andreisolcan@protonmail.com>
2022-12-05 21:51:54 +0200

    11

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -65,17 +65,17 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /*
 /////////////////////////////////////////////////
-// Simple array methods
+// 🔴Simple array methods
 
 let arr = [`a`, `b`, `c`, `d`, `e`];
 
@@ -109,5 +109,70 @@ console.log([...arr, ...arr2]);
 // Join
 console.log(letters.join(` - `));
 */
+
+/*
 /////////////////////////////////////////////////
-// The new at method
+// 🔴The new at method     .at()
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting the last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log(`jonas`.at(0));
+console.log(`jonas`.at(-1));
+*/
+
+/*
+/////////////////////////////////////////////////
+//🌟🔴🌟 Looping arrays: forEach()
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log(`---------------------FOREACH`);
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+*/
+/*
+/////////////////////////////////////////////////
+// 🔴 forEach with maps and sets
+
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set - key is the same as value (set has no keys)
+const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _value, map) {
+  console.log(`${value}: ${value}`);
+});
+*/
+
+/////////////////////////////////////////////////
+// 🔴 Project: "Bankist" app
