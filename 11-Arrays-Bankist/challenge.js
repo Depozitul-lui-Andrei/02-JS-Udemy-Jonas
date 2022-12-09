c9661d86d9a8c4ebec742fd1f92c56715bed4258
Andrei Solcan <andreisolcan@protonmail.com>
2022-12-09 23:52:47 +0200

    11

--- 11-Arrays-Bankist/challenge.js
+++ 11-Arrays-Bankist/challenge.js
@@ -100,6 +100,7 @@ calcAverageHumanAge2(dogs1);
 calcAverageHumanAge2(dogs2);
 */
 
+/*
 `Coding Challenge #3
 Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
 as an arrow function, and using chaining!
@@ -118,3 +119,14 @@ const calcAverageHumanAge3 = (ages) => {
 };
 calcAverageHumanAge3([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge3([16, 6, 10, 5, 6, 1, 4]);
+
+//Jonas
+const calcAverageHumanAge4 = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter((age) => age > 18)
+    .reduce((acc, age, i, arr) => (acc += age / arr.length), 0);
+
+console.log(calcAverageHumanAge4([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge4([16, 6, 10, 5, 6, 1, 4]));
+*/
