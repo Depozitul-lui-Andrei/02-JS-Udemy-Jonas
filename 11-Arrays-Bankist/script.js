cc5a62f9b97efee34a52429cd3ee1346551eaffa
Andrei Solcan <andreisolcan@protonmail.com>
2022-12-12 22:54:18 +0200

    11

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -499,3 +499,25 @@ console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 */
+
+/////////////////////////////////////////////////
+// 🔴 Flat and flatmap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat()); // all numbers are in one array
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(1)); // 1 depth is default
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap - map and flat combined, but flat goes only 1 level deep and can't be changed
+const overalBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
