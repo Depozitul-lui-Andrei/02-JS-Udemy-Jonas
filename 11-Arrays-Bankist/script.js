c6a036ac0afba35b4c8c3533d3e54cc877e21082
Andrei Solcan <andreisolcan@protonmail.com>
2022-12-13 15:56:40 +0200

    11

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -69,7 +69,7 @@ const displayMovements = function (movements, sort = false) {
 
   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
-  movements.forEach(function (mov, i) {
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? `deposit` : `withdrawal`;
 
     const html = `
@@ -230,6 +230,7 @@ let sorted = false;
 btnSort.addEventListener(`click`, function (e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
+
   sorted = !sorted;
 });
 /////////////////////////////////////////////////
@@ -564,4 +565,49 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+/*
+/////////////////////////////////////////////////
+// 🔴 More ways of creating and filling arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+
+x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+// small challenge
+const diceRolls = Array.from({ length: 100 }, () =>
+  Math.trunc(Math.random() * 6 + 1)
+);
+console.log(diceRolls);
+
+// real usecase
+
+labelBalance.addEventListener(`click`, function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll(`.movements__value`),
+    (el) => Number(el.textContent.replace(`€`, ``))
+  );
+
+  console.log(movementsUI);
+
+  movementsUI2 = [...document.querySelectorAll(`.movements__value`)];
+});
 */
