07b7b4ab61f7b56aa10d0252c6b1a8518ee57e74
Andrei <andreisolcan@protonmail.com>
2022-12-22 23:33:24 +0200

    12

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -431,3 +431,41 @@ console.log(huge + ` is REALLY BIG!!!`);
 console.log(11n / 3n);
 console.log(10 / 3);
 */
+
+/*
+/////////////////////////////////////////////////
+// 🔴 Creating dates
+
+// Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date(`Dec 22 2022 23:18:36`));
+console.log(new Date(`December 25, 2015`));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 33));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142249780000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
