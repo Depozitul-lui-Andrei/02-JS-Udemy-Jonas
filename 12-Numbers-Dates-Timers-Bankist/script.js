d72cb5c6d6321b507d18ab1779f6235ee6a5a9d2
Andrei Solcan <andreisolcan@protonmail.com>
2022-12-25 10:14:40 +0200

    12

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -81,7 +81,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
@@ -92,10 +92,11 @@ const formatMovementDate = function (date) {
   if (daysPassed === 1) return `Yesterday`;
   if (daysPassed <= 7) return `${daysPassed} days ago`;
   else {
-    const day = `${date.getDate()}`.padStart(2, `0`);
-    const month = `${date.getMonth() + 1}`.padStart(2, `0`);
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    // const day = `${date.getDate()}`.padStart(2, `0`);
+    // const month = `${date.getMonth() + 1}`.padStart(2, `0`);
+    // const year = date.getFullYear();
+    // return `${day}/${month}/${year}`;
+    return new Intl.DateTimeFormat(locale).format(date);
   }
 };
 
@@ -110,7 +111,8 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -183,8 +185,6 @@ currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
-// day/month/year
-
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
@@ -203,13 +203,28 @@ btnLogin.addEventListener('click', function (e) {
 
     // Create current date
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, `0`);
-    const month = `${now.getMonth() + 1}`.padStart(2, `0`);
-    const year = now.getFullYear();
-    const hour = `${now.getHours()}`.padStart(2, `0`);
-    const min = `${now.getMinutes()}`.padStart(2, `0`);
+    const options = {
+      hour: `numeric`,
+      minute: `numeric`,
+      day: `numeric`,
+      month: `numeric`,
+      year: `numeric`,
+    };
+    // const locale = navigator.language;
+    // console.log(locale);
 
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+    // const now = new Date();
+    // const day = `${now.getDate()}`.padStart(2, `0`);
+    // const month = `${now.getMonth() + 1}`.padStart(2, `0`);
+    // const year = now.getFullYear();
+    // const hour = `${now.getHours()}`.padStart(2, `0`);
+    // const min = `${now.getMinutes()}`.padStart(2, `0`);
+
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -532,3 +547,6 @@ const calcDaysPassed = (date1, date2) =>
 const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
 */
+
+/////////////////////////////////////////////////
+// 🔴 Internationalizing dates
