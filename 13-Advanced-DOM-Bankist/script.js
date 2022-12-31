fef72ca9d50b0531267ab101088abeb29ac90411
Andrei Solcan <andreisolcan@protonmail.com>
2022-12-31 20:45:07 +0200

    13

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -37,4 +37,44 @@ document.addEventListener('keydown', function (e) {
 // 🔴 How the DOM really works
 
 /////////////////////////////////////////////////
-// 🔴 Selecting, creating and deleting elements
+// 🔴🔴🔴 Selecting, creating and deleting elements
+
+/*
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(`.header`);
+const allSections = document.querySelectorAll(`.section`);
+console.log(allSections);
+
+document.getElementById(`section--1`);
+const allButtons = document.getElementsByTagName(`button`);
+console.log(allButtons);
+
+console.log(document.getElementsByClassName(`btn`));
+
+// Creating and inserting elements
+// .insertAdjacentHTML - in bankist app
+
+const message = document.createElement(`div`);
+message.classList.add(`cookie-message`);
+// message.textContent = `We use cookies for improved functionality and analytics.`;
+message.innerHTML = `We use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`;
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector(`.btn--close-cookie`)
+  .addEventListener(`click`, function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
+*/
