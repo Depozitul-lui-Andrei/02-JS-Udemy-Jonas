a3e16d1e12d6873c6a2dc02d595ee8f87651e675
Andrei <andreisolcan@protonmail.com>
2023-01-22 23:35:29 +0200

    13

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -189,6 +189,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal sections
+const allSections = document.querySelectorAll(`.section`);
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove(`section--hidden`);
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add(`section--hidden`);
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -396,3 +417,6 @@ console.log(h1.parentElement.children);
 
 /////////////////////////////////////////////////
 // 🔴 A better way: the intersection observer api
+
+/////////////////////////////////////////////////
+// 🔴 Revealing elements on scroll
