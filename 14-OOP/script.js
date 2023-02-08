597d9e192b8ef60a71e50824db10e68f7b8f8359
Andrei <andreisolcan@protonmail.com>
2023-02-08 23:23:33 +0200

    14

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -7,6 +7,7 @@
 ///////////////////////////////////////////////////
 // 🔴 OOP in JavaScript
 
+// /*
 ///////////////////////////////////////////////////
 // 🔴 Constructor functions and the new operators
 
@@ -38,3 +39,30 @@ const jay = `Jay`;
 
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
+// */
+
+///////////////////////////////////////////////////
+// 🔴 Prototypes (prototype of linked objects)
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = `Homo Sapiens`;
+console.log(jonas);
+console.log(matilda);
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty(`firstName`));
+console.log(jonas.hasOwnProperty(`species`));
