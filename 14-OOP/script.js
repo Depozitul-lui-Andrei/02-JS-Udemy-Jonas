f6bba8356efbb9ff3878acf4237edc4de7a86b42
Andrei <andreisolcan@protonmail.com>
2023-02-15 23:43:52 +0200

    14

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -99,3 +99,44 @@ console.log(arr.unique());
 const h1 = document.querySelector(`h1`);
 console.dir(h1); // check __proto__ how many they are?
 console.dir((x) => x + 1);
+
+///////////////////////////////////////////////////
+// 🔴 Coding Challenge #1
+`Coding Challenge #1
+Your tasks:
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+GOOD LUCK �
+`;
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  return this.speed;
+};
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  return this.speed;
+};
+
+const bmw = new Car(`BMW`, 120);
+console.log(bmw);
+console.log(bmw.accelerate());
+console.log(bmw.accelerate());
+console.log(bmw.accelerate());
+console.log(bmw.brake());
+
+const mercedes = new Car(`Mercedes`, 95);
+console.log(mercedes);
+console.log(mercedes.brake());
+console.log(mercedes.brake());
+console.log(mercedes.accelerate());
