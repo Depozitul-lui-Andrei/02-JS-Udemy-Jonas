984168c5a89710ec0256d3f1d280fa7d71882c62
Andrei Solcan <andreisolcan@protonmail.com>
2023-02-16 23:00:24 +0200

    14

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -164,3 +164,42 @@ bmw2.accelerate();
 bmw2.accelerate();
 bmw2.brake();
 bmw2.accelerate();
+
+///////////////////////////////////////////////////
+// 🔴 ES6 classes
+// classes are still functions
+
+// class expression
+// const PersonCls = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl(`Jessica`, 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
