b349fe08bb8f1c07c2dc5417b1d6ef45551e4c18
Andrei <andreisolcan@protonmail.com>
2023-03-30 22:30:32 +0300

    14

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -418,3 +418,69 @@ Tesla.chargeBattery(90);
 console.log(Tesla);
 Tesla.accelerate();
 Tesla.accelerate();
+
+///////////////////////////////////////////////////
+// 🔴 Inherintance between 'classes': ES6 Classes
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Whenever we try to set a property that already exists... use _ before property
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(` `)) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+  // static method
+  static hey() {
+    console.log(`Hey there 👋`);
+    console.log(this);
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear); // super is the constructor function of the parent class
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+// const martha = new StudentCl(`Martha Jones`, 2012);
+const martha = new StudentCl(`Martha Jones`, 2012, `Computer Science`);
+martha.introduce();
+martha.calcAge();
